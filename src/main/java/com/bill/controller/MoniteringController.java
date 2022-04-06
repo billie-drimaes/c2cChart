dbc629bb5a5550bb6a36f53520b5e668ae36edf6
billie-drimaes <billie@drimaes.com>
2022-04-06 18:50:40 +0900

    Revert "commit_charts"
    This reverts commit 18f577a8ddfb2e6ebdf8b0b258186830bf7a0ba8.

--- src/main/java/com/bill/controller/MoniteringController.java
+++ src/main/java/com/bill/controller/MoniteringController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MonitoringController {
+public class MoniteringController {
     
     
     @RequestMapping("/monitering")
